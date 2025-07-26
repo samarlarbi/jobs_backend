09a8798ab65b112b572173675203d4ab8ea2bcbd
samarlarbi <samarlarbi2004@gmail.com>
2025-07-26 17:39:05 +0100

    '.'

--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -1,7 +1,10 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 import * as dotenv from 'dotenv';
 
+
+
 dotenv.config();
+console.log(process.env.DATABASE);
 export const pgConfig: PostgresConnectionOptions = {
   type: 'postgres',
   url:  process.env.DATABASE,
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
